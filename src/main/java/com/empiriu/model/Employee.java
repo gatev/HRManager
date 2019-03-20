48bf66c07f87d2a072dc1abbb076fd0743e970aa
gatev <hristo_6@mail.bg>
2019-03-20 11:11:48 +0200

    Delete empty row after test

--- src/main/java/com/empiriu/model/Employee.java
+++ src/main/java/com/empiriu/model/Employee.java
@@ -24,7 +24,6 @@ import java.util.Set;
         })
 })
 public class Employee implements Serializable {
-    
     @Id
     @GeneratedValue(strategy = GenerationType.IDENTITY)
     @Column(name = "employee_id")
